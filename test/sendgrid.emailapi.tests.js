a37b44621b4676b7e78761e5e8f8bfbf0d522de7
Srinivas Sunkari <srinivas.sunkari@laterooms.com>
2016-02-26 21:35:26 +0000

    Removed send grid api key

--- test/sendgrid.emailapi.tests.js
+++ test/sendgrid.emailapi.tests.js
@@ -1,6 +1,6 @@
 describe.skip('send email using send grid', function () {
     it('should send email message', function (done) {
-        var sendgrid = require('sendgrid')('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');
+        var sendgrid = require('sendgrid')('*****');
         sendgrid.send({
             to: 'sr.sunkari@gmail.com',
             from: 'rent-portal@herokuapp.com',
